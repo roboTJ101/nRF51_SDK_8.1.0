2d7b53d9db7678cbbd652373e545841023e2c7e2
roboTJ101 <tjwatson@wpi.edu>
2015-06-10 16:37:05 -0400

    Removed DFU and commented out code

--- examples/ble_peripheral/ble_app_bas/main.c
+++ examples/ble_peripheral/ble_app_bas/main.c
@@ -64,18 +64,6 @@
 #define MAX_BATTERY_LEVEL                100                                        /**< Maximum simulated battery level. */
 #define BATTERY_LEVEL_INCREMENT          1                                          /**< Increment between each simulated battery level measurement. */
 
-//#define HEART_RATE_MEAS_INTERVAL         APP_TIMER_TICKS(1000, APP_TIMER_PRESCALER) /**< Heart rate measurement interval (ticks). */
-//#define MIN_HEART_RATE                   140                                        /**< Minimum heart rate as returned by the simulated measurement function. */
-//#define MAX_HEART_RATE                   300                                        /**< Maximum heart rate as returned by the simulated measurement function. */
-//#define HEART_RATE_INCREMENT             10                                         /**< Value by which the heart rate is incremented/decremented for each call to the simulated measurement function. */
-
-//#define RR_INTERVAL_INTERVAL             APP_TIMER_TICKS(300, APP_TIMER_PRESCALER)  /**< RR interval interval (ticks). */
-//#define MIN_RR_INTERVAL                  100                                        /**< Minimum RR interval as returned by the simulated measurement function. */
-//#define MAX_RR_INTERVAL                  500                                        /**< Maximum RR interval as returned by the simulated measurement function. */
-//#define RR_INTERVAL_INCREMENT            1                                          /**< Value by which the RR interval is incremented/decremented for each call to the simulated measurement function. */
-
-//#define SENSOR_CONTACT_DETECTED_INTERVAL APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER) /**< Sensor Contact Detected toggle interval (ticks). */
-
 #define MIN_CONN_INTERVAL                MSEC_TO_UNITS(400, UNIT_1_25_MS)           /**< Minimum acceptable connection interval (0.4 seconds). */
 #define MAX_CONN_INTERVAL                MSEC_TO_UNITS(650, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (0.65 second). */
 #define SLAVE_LATENCY                    0                                          /**< Slave latency. */
@@ -93,43 +81,18 @@
 #define SEC_PARAM_MAX_KEY_SIZE           16                                         /**< Maximum encryption key size. */
 
 #define DEAD_BEEF                        0xDEADBEEF                                 /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
-#ifdef BLE_DFU_APP_SUPPORT
-#define DFU_REV_MAJOR                    0x00                                       /** DFU Major revision number to be exposed. */
-#define DFU_REV_MINOR                    0x01                                       /** DFU Minor revision number to be exposed. */
-#define DFU_REVISION                     ((DFU_REV_MAJOR << 8) | DFU_REV_MINOR)     /** DFU Revision number to be exposed. Combined of major and minor versions. */
-#define APP_SERVICE_HANDLE_START         0x000C                                     /**< Handle of first application specific service when when service changed characteristic is present. */
-#define BLE_HANDLE_MAX                   0xFFFF                                     /**< Max handle value in BLE. */
-
-STATIC_ASSERT(IS_SRVC_CHANGED_CHARACT_PRESENT);                                     /** When having DFU Service support in application the Service Changed Characteristic should always be present. */
-#endif // BLE_DFU_APP_SUPPORT
-
 
 static uint16_t                          m_conn_handle = BLE_CONN_HANDLE_INVALID;   /**< Handle of the current connection. */
 static ble_bas_t                         m_bas;                                     /**< Structure used to identify the battery service. */
-//static ble_hrs_t                         m_hrs;                                     /**< Structure used to identify the heart rate service. */
-//static bool                              m_rr_interval_enabled = false;              /**< Flag for enabling and disabling the registration of new RR interval measurements (the purpose of disabling this is just to test sending HRM without RR interval data. */
 
 static sensorsim_cfg_t                   m_battery_sim_cfg;                         /**< Battery Level sensor simulator configuration. */
 static sensorsim_state_t                 m_battery_sim_state;                       /**< Battery Level sensor simulator state. */
-//static sensorsim_cfg_t                   m_heart_rate_sim_cfg;                      /**< Heart Rate sensor simulator configuration. */
-//static sensorsim_state_t                 m_heart_rate_sim_state;                    /**< Heart Rate sensor simulator state. */
-//static sensorsim_cfg_t                   m_rr_interval_sim_cfg;                     /**< RR Interval sensor simulator configuration. */
-//static sensorsim_state_t                 m_rr_interval_sim_state;                   /**< RR Interval sensor simulator state. */
 
 static app_timer_id_t                    m_battery_timer_id;                        /**< Battery timer. */
-//static app_timer_id_t                    m_heart_rate_timer_id;                     /**< Heart rate measurement timer. */
-//static app_timer_id_t                    m_rr_interval_timer_id;                    /**< RR interval timer. */
-//static app_timer_id_t                    m_sensor_contact_timer_id;                 /**< Sensor contact detected timer. */
 
 static dm_application_instance_t         m_app_handle;                              /**< Application identifier allocated by device manager */
 
-static ble_uuid_t m_adv_uuids[] = {/*{BLE_UUID_HEART_RATE_SERVICE,         BLE_UUID_TYPE_BLE},
-                                   */{BLE_UUID_BATTERY_SERVICE,            BLE_UUID_TYPE_BLE}/*,
-                                   {BLE_UUID_DEVICE_INFORMATION_SERVICE, BLE_UUID_TYPE_BLE}*/}; /**< Universally unique service identifiers. */
-#ifdef BLE_DFU_APP_SUPPORT    
-static ble_dfu_t                         m_dfus;                                    /**< Structure used to identify the DFU service. */
-#endif // BLE_DFU_APP_SUPPORT    
-
+static ble_uuid_t m_adv_uuids[] = {{BLE_UUID_BATTERY_SERVICE,            BLE_UUID_TYPE_BLE}}; /**< Universally unique service identifiers. */
 
 /**@brief Callback function for asserts in the SoftDevice.
  *
@@ -183,83 +146,6 @@ static void battery_level_meas_timeout_handler(void * p_context)
     battery_level_update();
 }
 
-
-/**@brief Function for handling the Heart rate measurement timer timeout.
- *
- * @details This function will be called each time the heart rate measurement timer expires.
- *          It will exclude RR Interval data from every third measurement.
- *
- * @param[in] p_context  Pointer used for passing some arbitrary information (context) from the
- *                       app_start_timer() call to the timeout handler.
- */
-/*static void heart_rate_meas_timeout_handler(void * p_context)*/
-/*{*/
-/*    static uint32_t cnt = 0;*/
-/*    uint32_t        err_code;*/
-/*    uint16_t        heart_rate;*/
-
-/*    UNUSED_PARAMETER(p_context);*/
-
-/*    heart_rate = (uint16_t)sensorsim_measure(&m_heart_rate_sim_state, &m_heart_rate_sim_cfg);*/
-
-/*    cnt++;*/
-/*    err_code = ble_hrs_heart_rate_measurement_send(&m_hrs, heart_rate);*/
-/*    if ((err_code != NRF_SUCCESS) &&*/
-/*        (err_code != NRF_ERROR_INVALID_STATE) &&*/
-/*        (err_code != BLE_ERROR_NO_TX_BUFFERS) &&*/
-/*        (err_code != BLE_ERROR_GATTS_SYS_ATTR_MISSING)*/
-/*        )*/
-/*    {*/
-/*        APP_ERROR_HANDLER(err_code);*/
-/*    }*/
-
-/*    // Disable RR Interval recording every third heart rate measurement.*/
-/*    // NOTE: An application will normally not do this. It is done here just for testing generation*/
-/*    //       of messages without RR Interval measurements.*/
-/*    //m_rr_interval_enabled = ((cnt % 3) != 0);*/
-/*}*/
-
-
-/**@brief Function for handling the RR interval timer timeout.
- *
- * @details This function will be called each time the RR interval timer expires.
- *
- * @param[in] p_context  Pointer used for passing some arbitrary information (context) from the
- *                       app_start_timer() call to the timeout handler.
- */
-/*static void rr_interval_timeout_handler(void * p_context)*/
-/*{*/
-/*    UNUSED_PARAMETER(p_context);*/
-
-/*    if (m_rr_interval_enabled)*/
-/*    {*/
-/*        uint16_t rr_interval;*/
-
-/*        rr_interval = (uint16_t)sensorsim_measure(&m_rr_interval_sim_state,*/
-/*                                                      &m_rr_interval_sim_cfg);*/
-/*        ble_hrs_rr_interval_add(&m_hrs, rr_interval);*/
-/*    }*/
-/*}*/
-
-
-/**@brief Function for handling the Sensor Contact Detected timer timeout.
- *
- * @details This function will be called each time the Sensor Contact Detected timer expires.
- *
- * @param[in] p_context  Pointer used for passing some arbitrary information (context) from the
- *                       app_start_timer() call to the timeout handler.
- */
-/*static void sensor_contact_detected_timeout_handler(void * p_context)*/
-/*{*/
-/*    static bool sensor_contact_detected = false;*/
-
-/*    UNUSED_PARAMETER(p_context);*/
-
-/*    sensor_contact_detected = !sensor_contact_detected;*/
-/*    ble_hrs_sensor_contact_detected_update(&m_hrs, sensor_contact_detected);*/
-/*}*/
-
-
 /**@brief Function for the Timer initialization.
  *
  * @details Initializes the timer module. This creates and starts application timers.
@@ -276,21 +162,6 @@ static void timers_init(void)
                                 APP_TIMER_MODE_REPEATED,
                                 battery_level_meas_timeout_handler);
     APP_ERROR_CHECK(err_code);
-
-/*    err_code = app_timer_create(&m_heart_rate_timer_id,*/
-/*                                APP_TIMER_MODE_REPEATED,*/
-/*                                heart_rate_meas_timeout_handler);*/
-/*    APP_ERROR_CHECK(err_code);*/
-
-/*    err_code = app_timer_create(&m_rr_interval_timer_id,*/
-/*                                APP_TIMER_MODE_REPEATED,*/
-/*                                rr_interval_timeout_handler);*/
-/*    APP_ERROR_CHECK(err_code);*/
-
-/*    err_code = app_timer_create(&m_sensor_contact_timer_id,*/
-/*                                APP_TIMER_MODE_REPEATED,*/
-/*                                sensor_contact_detected_timeout_handler);*/
-/*    APP_ERROR_CHECK(err_code);*/
 }
 
 
@@ -326,106 +197,6 @@ static void gap_params_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
-
-#ifdef BLE_DFU_APP_SUPPORT
-/**@brief Function for stopping advertising.
- */
-static void advertising_stop(void)
-{
-    uint32_t err_code;
-
-    err_code = sd_ble_gap_adv_stop();
-    APP_ERROR_CHECK(err_code);
-
-    err_code = bsp_indication_set(BSP_INDICATE_IDLE);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for loading application-specific context after establishing a secure connection.
- *
- * @details This function will load the application context and check if the ATT table is marked as 
- *          changed. If the ATT table is marked as changed, a Service Changed Indication
- *          is sent to the peer if the Service Changed CCCD is set to indicate.
- *
- * @param[in] p_handle The Device Manager handle that identifies the connection for which the context 
- *                     should be loaded.
- */
-static void app_context_load(dm_handle_t const * p_handle)
-{
-    uint32_t                 err_code;
-    static uint32_t          context_data;
-    dm_application_context_t context;
-
-    context.len    = sizeof(context_data);
-    context.p_data = (uint8_t *)&context_data;
-
-    err_code = dm_application_context_get(p_handle, &context);
-    if (err_code == NRF_SUCCESS)
-    {
-        // Send Service Changed Indication if ATT table has changed.
-        if ((context_data & (DFU_APP_ATT_TABLE_CHANGED << DFU_APP_ATT_TABLE_POS)) != 0)
-        {
-            err_code = sd_ble_gatts_service_changed(m_conn_handle, APP_SERVICE_HANDLE_START, BLE_HANDLE_MAX);
-            if ((err_code != NRF_SUCCESS) &&
-                (err_code != BLE_ERROR_INVALID_CONN_HANDLE) &&
-                (err_code != NRF_ERROR_INVALID_STATE) &&
-                (err_code != BLE_ERROR_NO_TX_BUFFERS) &&
-                (err_code != NRF_ERROR_BUSY) &&
-                (err_code != BLE_ERROR_GATTS_SYS_ATTR_MISSING))
-            {
-                APP_ERROR_HANDLER(err_code);
-            }
-        }
-
-        err_code = dm_application_context_delete(p_handle);
-        APP_ERROR_CHECK(err_code);
-    }
-    else if (err_code == DM_NO_APP_CONTEXT)
-    {
-        // No context available. Ignore.
-    }
-    else
-    {
-        APP_ERROR_HANDLER(err_code);
-    }
-}
-
-
-/** @snippet [DFU BLE Reset prepare] */
-/**@brief Function for preparing for system reset.
- *
- * @details This function implements @ref dfu_app_reset_prepare_t. It will be called by 
- *          @ref dfu_app_handler.c before entering the bootloader/DFU.
- *          This allows the current running application to shut down gracefully.
- */
-static void reset_prepare(void)
-{
-    uint32_t err_code;
-
-    if (m_conn_handle != BLE_CONN_HANDLE_INVALID)
-    {
-        // Disconnect from peer.
-        err_code = sd_ble_gap_disconnect(m_conn_handle, BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
-        APP_ERROR_CHECK(err_code);
-        err_code = bsp_indication_set(BSP_INDICATE_IDLE);
-        APP_ERROR_CHECK(err_code);
-    }
-    else
-    {
-        // If not connected, the device will be advertising. Hence stop the advertising.
-        advertising_stop();
-    }
-
-    err_code = ble_conn_params_stop();
-    APP_ERROR_CHECK(err_code);
-
-    nrf_delay_ms(500);
-}
-/** @snippet [DFU BLE Reset prepare] */
-#endif // BLE_DFU_APP_SUPPORT
-
-
 /**@brief Function for initializing services that will be used by the application.
  *
  * @details Initialize the Heart Rate, Battery and Device Information services.
@@ -433,30 +204,7 @@ static void reset_prepare(void)
 static void services_init(void)
 {
     uint32_t       err_code;
-/*    ble_hrs_init_t hrs_init;*/
     ble_bas_init_t bas_init;
-/*    ble_dis_init_t dis_init;*/
-/*    uint8_t        body_sensor_location;*/
-
-    // Initialize Heart Rate Service.
-/*    body_sensor_location = BLE_HRS_BODY_SENSOR_LOCATION_FINGER;*/
-
-/*    memset(&hrs_init, 0, sizeof(hrs_init));*/
-
-/*    hrs_init.evt_handler                 = NULL;*/
-/*    hrs_init.is_sensor_contact_supported = false;*/
-/*    hrs_init.p_body_sensor_location      = &body_sensor_location;*/
-
-/*    // Here the sec level for the Heart Rate Service can be changed/increased.*/
-/*    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&hrs_init.hrs_hrm_attr_md.cccd_write_perm);*/
-/*    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&hrs_init.hrs_hrm_attr_md.read_perm);*/
-/*    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&hrs_init.hrs_hrm_attr_md.write_perm);*/
-
-/*    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&hrs_init.hrs_bsl_attr_md.read_perm);*/
-/*    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&hrs_init.hrs_bsl_attr_md.write_perm);*/
-
-/*    err_code = ble_hrs_init(&m_hrs, &hrs_init);*/
-/*    APP_ERROR_CHECK(err_code);*/
 
     // Initialize Battery Service.
     memset(&bas_init, 0, sizeof(bas_init));
@@ -475,37 +223,6 @@ static void services_init(void)
 
     err_code = ble_bas_init(&m_bas, &bas_init);
     APP_ERROR_CHECK(err_code);
-
-/*    // Initialize Device Information Service.*/
-/*    memset(&dis_init, 0, sizeof(dis_init));*/
-
-/*    ble_srv_ascii_to_utf8(&dis_init.manufact_name_str, (char *)MANUFACTURER_NAME);*/
-
-/*    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&dis_init.dis_attr_md.read_perm);*/
-/*    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&dis_init.dis_attr_md.write_perm);*/
-
-/*    err_code = ble_dis_init(&dis_init);*/
-/*    APP_ERROR_CHECK(err_code);*/
-
-#ifdef BLE_DFU_APP_SUPPORT
-    /** @snippet [DFU BLE Service initialization] */
-    ble_dfu_init_t   dfus_init;
-
-    // Initialize the Device Firmware Update Service.
-    memset(&dfus_init, 0, sizeof(dfus_init));
-
-    dfus_init.evt_handler   = dfu_app_on_dfu_evt;
-    dfus_init.error_handler = NULL;
-    dfus_init.evt_handler   = dfu_app_on_dfu_evt;
-    dfus_init.revision      = DFU_REVISION;
-
-    err_code = ble_dfu_init(&m_dfus, &dfus_init);
-    APP_ERROR_CHECK(err_code);
-
-    dfu_app_reset_prepare_set(reset_prepare);
-    dfu_app_dm_appl_instance_set(m_app_handle);
-    /** @snippet [DFU BLE Service initialization] */
-#endif // BLE_DFU_APP_SUPPORT
 }
 
 
@@ -519,20 +236,6 @@ static void sensor_simulator_init(void)
     m_battery_sim_cfg.start_at_max = true;
 
     sensorsim_init(&m_battery_sim_state, &m_battery_sim_cfg);
-
-/*    m_heart_rate_sim_cfg.min          = MIN_HEART_RATE;*/
-/*    m_heart_rate_sim_cfg.max          = MAX_HEART_RATE;*/
-/*    m_heart_rate_sim_cfg.incr         = HEART_RATE_INCREMENT;*/
-/*    m_heart_rate_sim_cfg.start_at_max = false;*/
-
-/*    sensorsim_init(&m_heart_rate_sim_state, &m_heart_rate_sim_cfg);*/
-
-/*    m_rr_interval_sim_cfg.min          = MIN_RR_INTERVAL;*/
-/*    m_rr_interval_sim_cfg.max          = MAX_RR_INTERVAL;*/
-/*    m_rr_interval_sim_cfg.incr         = RR_INTERVAL_INCREMENT;*/
-/*    m_rr_interval_sim_cfg.start_at_max = false;*/
-
-/*    sensorsim_init(&m_rr_interval_sim_state, &m_rr_interval_sim_cfg);*/
 }
 
 
@@ -546,14 +249,6 @@ static void application_timers_start(void)
     err_code = app_timer_start(m_battery_timer_id, BATTERY_LEVEL_MEAS_INTERVAL, NULL);
     APP_ERROR_CHECK(err_code);
 
-/*    err_code = app_timer_start(m_heart_rate_timer_id, HEART_RATE_MEAS_INTERVAL, NULL);*/
-/*    APP_ERROR_CHECK(err_code);*/
-
-/*    err_code = app_timer_start(m_rr_interval_timer_id, RR_INTERVAL_INTERVAL, NULL);*/
-/*    APP_ERROR_CHECK(err_code);*/
-
-/*    err_code = app_timer_start(m_sensor_contact_timer_id, SENSOR_CONTACT_DETECTED_INTERVAL, NULL);*/
-/*    APP_ERROR_CHECK(err_code);*/
 }
 
 
@@ -697,11 +392,6 @@ static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
     ble_bas_on_ble_evt(&m_bas, p_ble_evt);
     ble_conn_params_on_ble_evt(p_ble_evt);
     bsp_btn_ble_on_ble_evt(p_ble_evt);
-#ifdef BLE_DFU_APP_SUPPORT
-    /** @snippet [Propagating BLE Stack events to DFU Service] */
-    ble_dfu_on_ble_evt(&m_dfus, p_ble_evt);
-    /** @snippet [Propagating BLE Stack events to DFU Service] */
-#endif // BLE_DFU_APP_SUPPORT
     on_ble_evt(p_ble_evt);
     ble_advertising_on_ble_evt(p_ble_evt);
 }
@@ -798,14 +488,6 @@ static uint32_t device_manager_evt_handler(dm_handle_t const * p_handle,
                                            ret_code_t        event_result)
 {
     APP_ERROR_CHECK(event_result);
-
-#ifdef BLE_DFU_APP_SUPPORT
-    if (p_event->event_id == DM_EVT_LINK_SECURED)
-    {
-        app_context_load(p_handle);
-    }
-#endif // BLE_DFU_APP_SUPPORT
-
     return NRF_SUCCESS;
 }
 
